9466aed780aa1fe7ab3bdaa8f26d3f365de54d1c
veroxzik <verox.zik@gmail.com>
2019-06-29 16:42:20 -0400

    Create Firmware Folder
    Arduino IDE will not load OpeNITHM.ino unless it's in a folder named OpeNITHM

---
+++ Firmware/OpeNITHM/Output.h
@@ -0,0 +1,13 @@
+// Output.h
+
+#ifndef _OUTPUT_h
+#define _OUTPUT_h
+
+class Output
+{
+  public:
+    virtual void sendKeyEvent(int key, bool pressed, bool doublePressed);
+    virtual void sendSensorEvent(float position);
+};
+
+#endif
---
+++ Firmware/OpeNITHM/USBOutput.h
@@ -0,0 +1,26 @@
+// USBOutput.h
+
+#ifndef _USBOUTPUT_h
+#define _USBOUTPUT_h
+
+#include "Output.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <HID-Project.h>
+
+class USBOutput : public Output
+{
+  private:
+    float lastPosition;
+  public:
+    void sendKeyEvent(int key, bool pressed, bool doublePressed) override;
+    void sendSensorEvent(float position) override;
+    USBOutput();
+};
+
+#endif
